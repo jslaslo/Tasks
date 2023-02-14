c705c8983433ff00d1ebc2e4a2722c38360f347e
jslaslo <125125092+jslaslo@users.noreply.github.com>
2023-02-14 13:39:59 +0200

    Tasks
    Tasks

---
+++ FigureLab/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();    
+                Console.WriteLine("Добро пожаловать в программу!! \nВыберете площадь какой фигуры Вы хотите посчитать?\n 1) Круга;\n 2) Треугольника.");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+                        Console.WriteLine("Введите радиус круга...");
+                        int radiusValue = Convert.ToInt32(Console.ReadLine());
+                        Circle circle = new Circle(radiusValue);
+                        Console.WriteLine($"Площадь круга = {circle.Square()}\nДля продолжения нажмите enter...");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        int sideOne, sideTwo, sideThree;
+                        Console.WriteLine("Введите значение стороны a = ...");
+                        sideOne = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Введите значение стороны b = ...");
+                        sideTwo = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Введите значение стороны c = ...");
+                        sideThree = Convert.ToInt32(Console.ReadLine());
+
+                        Triangle triangle = new Triangle(sideOne, sideTwo, sideThree);
+                        Console.WriteLine($"Площадь треугольника = {triangle.Square()}");
+                        Console.WriteLine($"Треугольник{(triangle.IsRight() ? " " : " не ")}прямоугольный\nДля продолжения нажмите enter...");
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректное число. Повторите попытку снова...\nДля продолжения нажмите enter...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+        
+}
---
+++ FigureLab/ShapeLab.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLab
+{
+    public abstract class Shape
+    {
+        public abstract double Square();
+    }
+    public class Circle : Shape
+    {
+        double radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public override double Square() => Math.PI * radius * radius;
+    }
+    public class Triangle : Shape
+    {
+        private readonly double _firstSide;
+        private readonly double _secondSide;
+        private readonly double _thirdSide;
+
+        public Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            _firstSide = firstSide;
+            _secondSide = secondSide;
+            _thirdSide = thirdSide;
+        }
+        public override double Square()
+        {
+            double semiPerimeter = (_firstSide + _secondSide + _thirdSide) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - _firstSide) * (semiPerimeter - _secondSide) * (semiPerimeter - _thirdSide));
+
+        }
+        public bool IsRight()
+        {
+            return ((Math.Pow(_firstSide, 2) + Math.Pow(_secondSide, 2)) == Math.Pow(_thirdSide, 2)) ||
+                    ((Math.Pow(_thirdSide, 2) + Math.Pow(_secondSide, 2)) == Math.Pow(_firstSide, 2)) ||
+                    ((Math.Pow(_firstSide, 2) + Math.Pow(_thirdSide, 2)) == Math.Pow(_secondSide, 2));
+            
+        }
+        
+    }
+}
+        
+
+       
+
+
+
